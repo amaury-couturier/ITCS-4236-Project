436a80d378384bd200fe71a8d9a22ed89089293c
Amaury Couturier <amo.couturier@gmail.com>
2023-10-28 17:59:29 -0400

    Coin
    tried to fix it but for some reason it just doesnt work

--- Assets/Scripts/CoinToss.cs
+++ Assets/Scripts/CoinToss.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinToss : Interactable
+public class CoinToss : MonoBehaviour
 {
     [SerializeField] private GameObject coin;
+    [SerializeField] public KeyCode interactKey;
     private GameObject instantiatedCoin; // Store a reference to the instantiated coin.
+    [SerializeField] private float radiusOfSatisfaction = 1.5f;
 
     private bool tossed = false;
 
@@ -16,10 +18,14 @@ public class CoinToss : Interactable
             tossed = true;
             instantiatedCoin = Instantiate(coin, transform.position, Quaternion.identity);
         }
-        if (inRange && Input.GetKeyDown(interactKey) && tossed)
+        
+        if (tossed)
         {
-            if (instantiatedCoin != null)
-            {
+            float distance = Vector3.Distance(transform.position, instantiatedCoin.transform.position);
+
+            if (instantiatedCoin != null && Input.GetKeyDown(interactKey) && distance <= radiusOfSatisfaction)
+            { 
+                tossed = false;
                 Destroy(instantiatedCoin); // Destroy the instantiated coin.
             }
         }
